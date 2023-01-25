05976dc86f1699664834c2f4c9e07f271601031e
Min-Jae Kim <94775103+GGamangCoder@users.noreply.github.com>
2023-01-25 15:51:32 +0900

    7576_토마토(수정코드)_MJ

--- BFS/7576_토마토/MJ.py
+++ BFS/7576_토마토/MJ.py
@@ -1,26 +1,25 @@
-# 왜 시간초과가 뜰까요 ?
-
-
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 # function declare
 def bfs():
     dx = [0, 0, 1, -1]
     dy = [1, -1, 0, 0]
+    global queue
     
     while queue:
-        x, y = queue.pop(0)
+        x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < M and 0 <= ny < N:
                 if graph[nx][ny] == 0:
                     queue.append((nx, ny))
-                    graph[nx][ny] = graph[x][y] + 1    
+                    graph[nx][ny] = graph[x][y] + 1
 
 # Input 
 N, M = map(int, input().split())
-graph, queue = [], []
+graph, queue = [], deque([])
 ans = 0
 
 for i in range(M):
