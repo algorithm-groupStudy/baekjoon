5b72c7fd9b0301faaa1a81ca177704414de388db
eunhee <6810779s@naver.com>
2023-01-31 10:29:05 +0900

    11054_eunhee_dp

--- DP/11054_가장긴바이토닉부분수열/eunhee.py
+++ DP/11054_가장긴바이토닉부분수열/eunhee.py
@@ -1,45 +1,20 @@
+# 나중에 다시 풀 문제 !!!! 
+
 a=int(input())
 arr=list(map(int,input().split()))
+arr_reverse=arr[::-1]
 dp_up=[1 for _ in range(a)]
 dp_down=[1 for _ in range(a)]
-
 for i in range(1,a):
     for j in range(i):
         if arr[i]>arr[j]:
             dp_up[i]=max(dp_up[j]+1,dp_up[i]) 
-        elif arr[i]<arr[j]:
+        if arr_reverse[i]>arr_reverse[j]: #뒤에서부터 감소하는 개수를 세줘야 되므로
             dp_down[i]=max(dp_down[j]+1,dp_down[i])
 
-print(dp_up)
-print(dp_down)
+arr.reverse()
 
-# def dp_up(lst):
-#     dp=[1 for _ in range(len(lst))]
-#     for i in range(1,len(lst)):
-#         for j in range(i):
-#             if lst[i]>lst[j]:
-#                 dp[i]=max(dp[j]+1,dp[i])
-#     return dp
-
-# def dp_down(lst):
-#     dp=[1 for _ in range(len(lst))]
-#     for i in range(1,len(lst)):
-#         for j in range(i):
-#             if lst[i]<lst[j]:
-#                 dp[i]=max(dp[j]+1,dp[i])
-#     return dp
-
-# dp_up=dp_up(arr)
-# dp_down=dp_down(arr)
-# res=0
-# for i in range(a-1):
-#     up=max(dp_up[:i+1])
-#     lst=dp_down[i+1:]
-#     print(lst)
-#     down=max(dp_up(lst))
-    
-#     res=max(res,up+down)
-
-# print(res)
-
-
+res=0
+for i in range(a):
+    res=max(res,dp_up[i]+dp_down[a-i-1])
+print(res-1) # -1은 중복되는 숫자 빼기
