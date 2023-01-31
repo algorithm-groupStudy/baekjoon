ffe2fb2ae580a1d9143d28198e85bd1a416e2cb1
JPark11 <iwri2089@gmail.com>
2023-01-31 10:00:30 +0900

    10815_숫자카드_jaeeun

---
+++ Binary/10815_숫자카드/jaeeun.py
@@ -0,0 +1,24 @@
+N = int(input())
+cards = list(map(int, input().split()))
+
+M = int(input())
+check = list(map(int, input().split()))
+
+def binary(num, arr):
+    l, r = 0, len(arr)-1 
+    while l <= r:
+        mid = (l + r) // 2 
+        if arr[mid] == num: 
+            return 1
+
+        elif arr[mid] < num:
+            l = mid + 1 
+        
+        elif arr[mid] > num:
+            r = mid - 1 
+
+    return 0
+
+cards.sort()
+for num in check:
+    print(binary(num, cards), end = ' ')  
