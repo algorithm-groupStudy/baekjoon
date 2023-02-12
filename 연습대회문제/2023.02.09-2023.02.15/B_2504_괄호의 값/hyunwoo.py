f47971dc22bbada4d8ce339bb5c68105f2a3c586
Hyunwoo Choi <coladrop93@gmail.com>
2023-02-12 23:40:32 +0900

    hyunwoo solved 2504

--- 연습대회문제/2023.02.09-2023.02.15/B_2504_괄호의 값/hyunwoo.py
+++ 연습대회문제/2023.02.09-2023.02.15/B_2504_괄호의 값/hyunwoo.py
@@ -1,42 +1,35 @@
-par_str = input().split()
+par_str = list(input())
 str_stack = []  # making a str_stack to check and pop from the str
-    
 i = 0 
 result = 0
 tmp = 1
-while i < len(par_str[0]):
-    print(f'{i} i')
-    if par_str[0][i] == "(":  #  if str is '(' than add it on the stack
-        str_stack.append(par_str[0][i])
+
+for i in range(len(par_str)):
+    if par_str[i] == "(":  #  if str is '(' than add it to the stack
+        str_stack.append(par_str[i])
         tmp *= 2
         
-    elif par_str[0][i] == "[":
-        str_stack.append(par_str[0][i])
+    elif par_str[i] == "[":
+        str_stack.append(par_str[i])
         tmp *= 3
-            
-    elif par_str[0][i] == ")":  # if face with ')' pop the top idx of the stack
-        if str_stack and str_stack[-1] == "(":  # Check if stack is not empty and last element is "("
-            print(tmp)
-            result += tmp
-        else:
-            result = 0
+       
+    elif par_str[i] == ")":  # if face with ')' pop the top idx of the stack
+        if not str_stack and str_stack[-1] == "[":  # Check if stack is not empty and last element is "["
             break
+        if par_str[i-1] == "(":
+            result += tmp
         str_stack.pop()
         tmp //= 2
                 
-    elif par_str[0][i] == "]":
-        if str_stack and str_stack[-1] == "[":  # Check if stack is not empty and last element is "["
-            result += tmp
-            print(tmp)
-        else:
-            result = 0
+    elif par_str[i] == "]":
+        if not str_stack and str_stack[-1] == "(":  # Check if stack is not empty and last element is "("
             break
+        if par_str[i-1] == "[":
+            result += tmp
         str_stack.pop()
         tmp //= 3
-    print(f'{result} result')
-    i += 1
         
 if str_stack:
-    result = 0
+    print(0)
 else:
     print(result)
