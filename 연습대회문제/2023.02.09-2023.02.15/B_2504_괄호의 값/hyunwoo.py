3a8164a3432f3507623f27ceec0b48917065518f
Hyunwoo Choi <coladrop93@gmail.com>
2023-02-12 18:37:42 +0900

    hyun woo sad

--- 연습대회문제/2023.02.09-2023.02.15/B_2504_괄호의 값/hyunwoo.py
+++ 연습대회문제/2023.02.09-2023.02.15/B_2504_괄호의 값/hyunwoo.py
@@ -1,25 +1,42 @@
-t = int(input())  # input number of test_case
-
-for test_case in range(1, t + 1): 
-    par_str = input().split()
-    str_stack = []  # making a str_stack to check and pop from the str
+par_str = input().split()
+str_stack = []  # making a str_stack to check and pop from the str
     
-    i = 0 
+i = 0 
+result = 0
+tmp = 1
+while i < len(par_str[0]):
+    print(f'{i} i')
+    if par_str[0][i] == "(":  #  if str is '(' than add it on the stack
+        str_stack.append(par_str[0][i])
+        tmp *= 2
+        
+    elif par_str[0][i] == "[":
+        str_stack.append(par_str[0][i])
+        tmp *= 3
+            
+    elif par_str[0][i] == ")":  # if face with ')' pop the top idx of the stack
+        if str_stack and str_stack[-1] == "(":  # Check if stack is not empty and last element is "("
+            print(tmp)
+            result += tmp
+        else:
+            result = 0
+            break
+        str_stack.pop()
+        tmp //= 2
+                
+    elif par_str[0][i] == "]":
+        if str_stack and str_stack[-1] == "[":  # Check if stack is not empty and last element is "["
+            result += tmp
+            print(tmp)
+        else:
+            result = 0
+            break
+        str_stack.pop()
+        tmp //= 3
+    print(f'{result} result')
+    i += 1
+        
+if str_stack:
     result = 0
-    while i < len(par_str[0]):
-        if par_str[0][i] == "(":  #  if str is '(' than add it on the stack
-            str_stack.append(par_str[0][i])
-        elif par_str[0][i] == ")":  # if face with ')' pop the top idx of the stack
-            if str_stack:
-                tmp = str_stack.pop() # to do this we need to check whether there is index in the stack
-                if tmp != "(":
-                    result += 3
-            else:
-                result = "NO"
-                break
-        i += 1
-        
-    if str_stack:
-        result = "NO"
-        
-    print(f"{result}")
+else:
+    print(result)
