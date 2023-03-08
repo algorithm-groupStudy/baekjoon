29bbcab186ca95fecfdeced1972b195f65729724
Hyunwoo <coladrop93@gmail.com>
2023-03-08 19:48:05 +0900

    hyunwoo

---
+++ 연습대회문제/2023.02.24-2023.03.09/B_우유 도시/hyunwoo.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open(r'/Users/hyunwoochoi/Desktop/python/algo/baekjoon/input.txt')
+
+number_of_stones = int(input())
+stones = list(map(int, input().split()))
+
+for i in range(1,n):
+    for j in range(
--- 연습대회문제/2023.03.06-2023.03.09/A_2468_안전영역/hyunwoo.py
+++ 연습대회문제/2023.03.06-2023.03.09/A_2468_안전영역/hyunwoo.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 field_size = int(input())
 field = [list(map(int, input().split())) for _ in range(field_size)]
 
-Graph = []
+graph = []
 result = 1 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
@@ -13,32 +13,40 @@ dy = [1, -1, 0, 0]
 # 최대 높이 찾기 
 max_height = 0
 for i in range(field_size):
-    graph.append(field[i])    
     for j in range(field_size):
         if field[i][j] >= max_height:
             max_height = field[i][j]
-        
 
+# 주변에 인접한 것이 없다면, visited 
 def dfs(x,y,num):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
     
         if field_size > nx >= 0 and field_size > ny >= 0 and visited[nx][ny] == 0:
-            if graph[nx][ny] > num:
+            if field[nx][ny] > num:
                 visited[nx][ny] = 1
                 dfs(nx, ny, num)
 
+# 0부터 최대 높이까지의 모든 안전영역의 갯수 세기 
+# for i in range(max_height):
+#     visited =  [[0] * field_size for _ in range(field_size)]
+#     cnt = 0  # 안전영역 갯수 초기화      
+#     for j in range(field_size):
+#         for k in range(field_size):
+#             if field[j][k] > i and visited[j][k] == 0:
+#                 cnt += 1
+#                 visited[j][k] = 1
+#                 dfs(j,k,i)
+#     result = max(result, cnt)
+
 for i in range(max_height):
     visited =  [[0] * field_size for _ in range(field_size)]
-    cnt = 0
-    
+    cnt = 0  # 안전영역 갯수 초기화      
     for j in range(field_size):
         for k in range(field_size):
-            if graph[j][k] > i and visited[j][k] == 0:
+            if field[j][k] > i and visited[j][k] == 0:
                 cnt += 1
-                visited[j][k] = 1
                 dfs(j,k,i)
     result = max(result, cnt)
-
 print(result)
