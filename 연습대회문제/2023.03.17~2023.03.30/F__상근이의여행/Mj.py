ae108aa567628f863154243ab6d92024fdb70743
unknown <zxskmj@gmail.com>
2023-03-28 09:07:07 +0900

    Rename_연습문제_230330

---
+++ 연습대회문제/2023.03.17~2023.03.30/F__상근이의여행/Mj.py
@@ -0,0 +1,47 @@
+# 상근이의 여행
+# DFS 풀이
+
+import sys
+input = sys.stdin.readline
+
+
+def dfs(idx, cnt):
+    visited[idx] = 1
+
+    for i in graph[idx]:
+        if not visited[i]:
+            cnt = dfs(i, cnt+1)
+    
+    return cnt
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    visited = [0] * (N+1)
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    print(dfs(1, 0))
+
+
+
+'''
+2번째 풀이 - 신장 트리
+
+import sys
+input = sys.stdin.readline
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    for _ in range(M):
+        input()
+
+    print(N-1)
+'''
