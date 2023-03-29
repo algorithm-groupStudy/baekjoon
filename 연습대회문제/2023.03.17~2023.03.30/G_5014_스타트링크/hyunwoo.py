75910c1266109ff2dacfeb100752cb33ca37361d
Hyunwoo <coladrop93@gmail.com>
2023-03-29 21:08:05 +0900

    hyun woo

--- 연습대회문제/2023.03.17~2023.03.30/G_5014_스타트링크/hyunwoo.py
+++ 연습대회문제/2023.03.17~2023.03.30/G_5014_스타트링크/hyunwoo.py
@@ -1,40 +1,26 @@
 import sys
 sys.stdin = open('input.txt')
 
-building_stairs, current_loc, loc_start, up, down = map(int, input().split())
+from collections import deque
 
-count = 0
+floors, start, goal, up, down = map(int, input().split())
 
-while True:
-    
-    # 스타트 링크가 up보다 위에 있을때는 up 한다.
-    if up != 0 and (loc_start - current_loc) // up > 0:
-        up_botton = (loc_start - current_loc) // up
-        count += up_botton
-        current_loc = current_loc + (up * up_botton)
-    
-        if current_loc == loc_start:
-            break    
+def bfs(start, goal):
+    q = deque()
+    visited = [-1] * (floors + 1) # 
+    q.append(start)
+    visited[start] = 0
 
-    # 스타트 링크가 아래에 있고, down 보다 작을때 
-    if down != 0 and (current_loc - loc_start) // down > 0:
-        down_botton = (current_loc - loc_start) // down 
-        count += down_botton
-        current_loc = current_loc + (down* down_botton)
+    while q:
+        x = q.popleft()
+        if x == goal:
+            return visited[x]
+        
+        for new_x in (x + up, x - down):
+            if 0 < new_x <= floors and visited[new_x] == -1:
+                q.append(new_x)
+                visited[new_x] = visited[x] + 1
+    return "use the stairs"
 
-        if current_loc == loc_start:
-            break
-    
-    # 스타트 링크가 
-    if abs(current_loc - loc_start) % abs(up-down) == 0:
-        double_botton = abs(current_loc-loc_start) // abs(up-down) 
-        count += double_botton * 2
-        break
 
-    count = -1
-    break
-
-if count == -1:
-    print("use the stairs")
-else:
-    print(count)
+print(bfs(start, goal))
